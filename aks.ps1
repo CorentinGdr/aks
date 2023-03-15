37bfa5d52a87830a17f8967d243156f938c5ca8c
CorentinGdr <108235756+CorentinGdr@users.noreply.github.com>
2023-03-15 19:02:44 +0100

    Add files via upload

---
+++ aks.ps1
@@ -0,0 +1,25 @@
+﻿param(
+    [string]$ResourceGroupName,
+    [string]$AksClusterName,
+    [string]$Action
+)
+
+if ($Action -ne "start" -and $Action -ne "stop") {
+    Write-Error "L'action doit être 'start' ou 'stop'"
+    exit
+}
+
+$ErrorActionPreference = "Stop"
+
+$connection = Get-AutomationConnection -Name 'AzureRunAsConnection'
+Connect-AzAccount -ServicePrincipal -TenantId $connection.TenantId -ApplicationId $connection.ApplicationId -CertificateThumbprint $connection.CertificateThumbprint
+
+$agentPoolName = (Get-AzAks -ResourceGroupName $ResourceGroupName -Name $AksClusterName).AgentPoolProfiles[0].Name
+
+if ($Action -eq "start") {
+    $nodeCount = 3
+} elseif ($Action -eq "stop") {
+    $nodeCount = 0
+}
+
+Set-AzAksNodePool -ResourceGroupName $ResourceGroupName -ClusterName $AksClusterName -Name $agentPoolName -NodeCount $nodeCount
